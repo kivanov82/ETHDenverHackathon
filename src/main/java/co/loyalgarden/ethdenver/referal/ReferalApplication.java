68717ef6a611581f37ae25169417086389042a91
Alex <blinovalexander3@gmail.com>
2018-02-17 15:09:17 +0100

    Spring boot added

---
+++ src/main/java/co/loyalgarden/ethdenver/referal/ReferalApplication.java
@@ -0,0 +1,12 @@
+package co.loyalgarden.ethdenver.referal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReferalApplication.class, args);
+	}
+}
