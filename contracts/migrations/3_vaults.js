8937ce86677a37a86ffeb61efd60c3fce63cb213
kirilivanov <k.ivanov@loyalgarden.co>
2018-02-17 18:56:59 -0700

    2 vaults for funds storage

--- contracts/migrations/3_vaults.js
+++ contracts/migrations/3_vaults.js
@@ -1,5 +1,6 @@
 var Vault = artifacts.require("./Vault.sol");
 
 module.exports = function(deployer) {
-    deployer.deploy(Vault);
+    deployer.deploy(Vault); //stability
+    deployer.deploy(Vault); //projects
 };
