8fc5e4dcbb93d8fec21e2e44a8f20350866b41d4
kirilivanov <k.ivanov@loyalgarden.co>
2018-02-17 22:34:26 -0700

    donate transaction

--- src/main/resources/static/js/const.js
+++ src/main/resources/static/js/const.js
@@ -5,4 +5,5 @@ const vaultProjectAddress = '0xd54b47f8e6a1b97f3a84f63c867286272b273b7c';
 
 const owner = '0x627306090abab3a6e1400e9345bc60c78a8bef57';
 const recipient = '0xf17f52151EbEF6C7334FAD080c5704D77216b732';
+const event = '0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef';
 
