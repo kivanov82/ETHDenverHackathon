85848bcd709c696ac2f4ea4b05792231303b2692
Alex <blinovalexander3@gmail.com>
2018-02-17 20:47:10 -0700

    vault update

--- src/main/resources/static/js/const.js
+++ src/main/resources/static/js/const.js
@@ -1,5 +1,6 @@
 const tokenAddress = '0x345ca3e014aaf5dca488057592ee47305d9b3e10';
-const vaultAddress = '0x8f0483125fcb9aaaefa9209d8e9d7b9c8b9fb90f';
+const vaultStabilityAddress = '0x75c35c980c0d37ef46df04d31a140b65503c0eed';
+const vaultProjectAddress = '0x82d50ad3c1091866e258fd0f1a7cc9674609d254';
 
 
 const owner = '0x627306090abab3a6e1400e9345bc60c78a8bef57';
